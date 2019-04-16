eebc990255700fca9be31154c101c6c20a585e4f
cyl5yy412 <cyl5yy412@gmail.com>
2019-04-16 14:59:47 +0800

    Initial commit

---
+++ src/main/java/com/sdsoon/TioChrHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.sdsoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TioChrHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TioChrHelloworldApplication.class, args);
+	}
+
+}
